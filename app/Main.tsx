5a39b16edde893daa4825a3446f9e6af12020b2a
erfur <erfur@mailbox.org>
2024-01-25 23:20:18 +0100

    better jump label

--- app/Main.tsx
+++ app/Main.tsx
@@ -57,9 +57,9 @@ export default function Home({ posts }) {
                         <Link
                           href={`/blog/${slug}`}
                           className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-                          aria-label={`Read more: "${title}"`}
+                          aria-label={`jmp: "${title}"`}
                         >
-                          Read more &rarr;
+                          jmp &rarr;
                         </Link>
                       </div>
                     </div>
