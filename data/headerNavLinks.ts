3d1fdbd37a4e75afd3455534ff8bde57c0043744
Furkan Er <mail@erfur.dev>
2026-06-01 23:34:32 +0200

    Replace Contact page with Labs link to erfurlabs.com

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/blog', title: 'Blog' },
   { href: '/projects', title: 'Projects' },
-  { href: '/contact', title: 'Contact' },
+  { href: 'https://erfurlabs.com', title: 'Labs' },
 ]
 
 export default headerNavLinks
